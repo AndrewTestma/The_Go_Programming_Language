3b295774f3bacaac192f345aa7b88edc141a194a
AndrewTestma <425780724@qq.com>
2019-12-04 23:27:18 +0800

    ex5.12

--- ch5/ex5.12/ex5.12.go
+++ ch5/ex5.12/ex5.12.go
@@ -7,6 +7,8 @@ import (
 	"os"
 )
 
+//练习5.12： gopl.io/ch5/outline2（5.5节）的startElement和endElement共用了全局变量depth，将它们修改为匿名函数，使其共享outline中的局部变量。
+
 func forEachNode(n *html.Node, pre, post func(n *html.Node)) {
 	if pre != nil {
 		pre(n)
