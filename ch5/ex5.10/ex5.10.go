05e4c447a03a87a67fe64f6eb4dac0d79c389129
AndrewTestma <425780724@qq.com>
2019-12-04 23:22:19 +0800

    ex5.10

---
+++ ch5/ex5.10/ex5.10.go
@@ -0,0 +1,48 @@
+package main
+
+import "fmt"
+
+//练习5.10：重写topoSort函数，用map代替切片并移除对key的排序代码。验证结果的正确性
+var prereqs = map[string][]string{
+	"algorithms": {"data structures"},
+	"calculus":   {"linear algebra"},
+	"compilers": {
+		"data structures",
+		"formal languages",
+		"computer organization",
+	},
+	"data structures":       {"discrete math"},
+	"databases":             {"data structures"},
+	"discrete math":         {"intro to programming"},
+	"formal languages":      {"discrete math"},
+	"networks":              {"operating systems"},
+	"operating systems":     {"data structures", "computer organization"},
+	"programming languages": {"data structures", "computer organization"},
+}
+
+func topoSort(m map[string][]string) []string {
+	var order []string
+	seen := make(map[string]bool)
+	var visitAll func(items []string)
+
+	visitAll = func(items []string) {
+		for _, item := range items {
+			if !seen[item] {
+				seen[item] = true
+				visitAll(m[item])
+				order = append(order, item)
+			}
+		}
+	}
+
+	for k := range m {
+		visitAll([]string{k})
+	}
+	return order
+}
+
+func main() {
+	for i, course := range topoSort(prereqs) {
+		fmt.Printf("%d:\t%s\n", i+1, course)
+	}
+}
