49d513230356a93b3c6c2b5b085f5e42c1d6727b
AndrewTestma <425780724@qq.com>
2019-12-11 00:24:19 +0800

    tempflag

---
+++ ch7/tempflag/tempflag.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"The_Go_Programming_Language/ch7/tempconv"
+	"flag"
+	"fmt"
+)
+
+var temp = tempconv.CelsiusFlag("temp", 20.0, "the temperature")
+
+func main() {
+	flag.Parse()
+	fmt.Println(*temp)
+}
