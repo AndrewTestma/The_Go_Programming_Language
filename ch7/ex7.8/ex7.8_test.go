a15277d6be370d9383147817d0496b75ce9156fc
AndrewTestma <425780724@qq.com>
2019-12-17 22:45:30 +0800

    练习 7.8：很多图形界面提供了一个有状态的多重排序表格插件：主要的排序键是最近一次点击过列头的列，第二个排序键是第二最近点击过列头的列，等等。定义一个sort.Interface的实现用在这样的表格中。比较这个实现方式和重复使用sort.Stable来排序的方式。

---
+++ ch7/ex7.8/ex7.8_test.go
@@ -0,0 +1,49 @@
+package ex7_8
+
+import (
+	"sort"
+	"testing"
+)
+
+func Cmp(a, b []Person, t *testing.T) {
+	if len(a) != len(b) {
+		t.Log("different lengths")
+		t.Logf("%s\n%s", a, b)
+		t.Fail()
+		return
+	}
+	for i := 0; i < len(a); i++ {
+		if a[i] != b[i] {
+			t.Logf("different elements, starting at %d", i)
+			t.Logf("%s\n%s", a, b)
+			t.Fail()
+			return
+		}
+	}
+}
+
+func TestByColumns_Age(t *testing.T) {
+	people := []Person{
+		{"Alice", 20},
+		{"Alice", 12},
+	}
+	c := &ByColumns{people, nil, 2}
+	c.Select(c.LessAge)
+	sort.Sort(c)
+	Cmp(people, []Person{
+		{"Alice", 12}, {"Alice", 20},
+	}, t)
+}
+
+func TestByColumns_Name(t *testing.T) {
+	people := []Person{
+		{"Alice", 20},
+		{"Bob", 20},
+	}
+	c := &ByColumns{people, nil, 2}
+	c.Select(c.LessName)
+	sort.Sort(c)
+	Cmp(people, []Person{
+		{"Bob", 20}, {"Alice", 20},
+	}, t)
+}
